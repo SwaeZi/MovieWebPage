fbb85f6b822a9cc8fb9d28d02d88ee982447a8b0
SwaeZi <stevenstefanov@icloud.com>
2023-09-25 12:08:36 +0200

    fix: landing page, feat:fetch movie data

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const fetchData = async () => {
-    const response = await axios.get();
-}
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '535e335d',
+            s: 'avengers'
+        }
+    });
+    console.log(response.data);
+};
+fetchData();
