2e640702b35323b19d2c1e417982a2a92e309aeb
SwaeZi <stevenstefanov@icloud.com>
2023-09-26 11:54:25 +0200

    feat: search input

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '535e335d',
-            s: 'avengers'
+            s: searchTerm
         }
     });
     console.log(response.data);
 };
-fetchData();
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+});
+
