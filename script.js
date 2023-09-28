97de778d78d4c3638b0c81a1e709feb1e01667bf
SwaeZi <stevenstefanov@icloud.com>
2023-09-28 13:27:41 +0200

    feat: dropdown menu

--- script.js
+++ script.js
@@ -5,23 +5,40 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
+    if (response.data.Error)
+    return [];
     return response.data.Search;
 };
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWr = document.querySelector('.results');
 
 const onInput = async event => {
    const movies = await fetchData(event.target.value);
    
+   resultsWr.innerHTML = '';
+   dropdown.classList.add('is-active');
    for (let movie of movies) {
-    const div = document.createElement('div');
+    const option = document.createElement('a');
 
-    div.innerHTML = `
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
     <img src="${movie.Poster}"/>
     <h1>${movie.Title}</h1>
     `;
     
-    document.querySelector('#target').appendChild(div);
+    resultsWr.appendChild(option);
    }
 }; 
 input.addEventListener('input', debounce(onInput, 1000));
