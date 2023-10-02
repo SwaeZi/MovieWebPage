2f8eb0e3ee019932836db525e5e4aacd10096a1a
SwaeZi <stevenstefanov@icloud.com>
2023-10-02 11:16:29 +0200

    feat: dislay movie details

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const onInput = async event => {
      option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
         input.value = movie.Title;
+        onMovieSelect(movie);
      });
 
 
@@ -59,3 +60,34 @@ document.addEventListener("click", event => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async movie => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "535e335d",
+        i: movie.imdbID
+      },
+    });
+
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+    return `
+    <article class="media">
+        <figure class="media-left">
+            <p class="image">
+                <img src="${movieDetail.Poster}" />
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetail.Title}</h1>
+                <h4>${movieDetail.Genre}</h4>
+                <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article>
+    
+    `;
+};
