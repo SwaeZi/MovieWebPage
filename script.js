ca7ba007be04c306ff22c9a3d691563a7843a036
SwaeZi <stevenstefanov@icloud.com>
2023-10-02 10:33:58 +0200

    update search input to the selected movie title

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ const resultsWr = document.querySelector('.results');
 const onInput = async event => {
    const movies = await fetchData(event.target.value);
    
+    if(!movies.length) {
+        dropdown.classList('is-active');
+        return;
+    }
+
    resultsWr.innerHTML = '';
    dropdown.classList.add('is-active');
    for (let movie of movies) {
@@ -38,9 +43,19 @@ const onInput = async event => {
     <img src="${movie.Poster}"/>
     ${movie.Title}
     `;
-    
+     option.addEventListener('click', () => {
+        dropdown.classList.remove('is-active');
+        input.value = movie.Title;
+     });
+
+
     resultsWr.appendChild(option);
    }
 }; 
 input.addEventListener('input', debounce(onInput, 1000));
 
+document.addEventListener("click", event => {
+  if(!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
