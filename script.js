e43cdc51f1bec61924bec025712c36ac92277b39
SwaeZi <stevenstefanov@icloud.com>
2023-10-03 14:31:29 +0200

    add: another 3 search inputs

--- script.js
+++ script.js
@@ -9,57 +9,18 @@ const fetchData = async (searchTerm) => {
     return [];
     return response.data.Search;
 };
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search for a Movie</b></label>
-<input class="input" />
-<div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-</div>
-`;
 
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWr = document.querySelector('.results');
-
-const onInput = async event => {
-   const movies = await fetchData(event.target.value);
-   
-    if(!movies.length) {
-        dropdown.classList('is-active');
-        return;
-    }
-
-   resultsWr.innerHTML = '';
-   dropdown.classList.add('is-active');
-   for (let movie of movies) {
-    const option = document.createElement('div');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-    <img src="${movie.Poster}"/>
-    ${movie.Title}
-    `;
-     option.addEventListener('click', () => {
-        dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        onMovieSelect(movie);
-     });
-
-
-    resultsWr.appendChild(option);
-   }
-}; 
-input.addEventListener('input', debounce(onInput, 1000));
-
-document.addEventListener("click", event => {
-  if(!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
+createAutocomplete({
+  root: document.querySelector(".autocomplete"),
 });
+createAutocomplete({
+  root: document.querySelector(".autocomplete-two"),
+});
+createAutocomplete({
+  root: document.querySelector(".autocomplete-three"),
+});
+
+
 
 const onMovieSelect = async movie => {
     const response = await axios.get("http://www.omdbapi.com/", {
@@ -69,7 +30,7 @@ const onMovieSelect = async movie => {
       },
     });
 
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
@@ -106,7 +67,7 @@ const movieTemplate = movieDetail => {
     </article>
     <article class="notification is-primary">
         <p class=title">${movieDetail.imdbVotes}</p>
-        <p class="subtitle">IMDB Votes</p>
+        <p class="subtitle">Awards</p>
     </article>
     `;
 };
